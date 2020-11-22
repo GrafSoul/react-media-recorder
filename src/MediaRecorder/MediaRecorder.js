534f93838a3a040489a7a7682f254be01e2c5cff
GrafSoul <grafsoul@gmail.com>
2020-11-22 16:07:28 +0500

    feat: Added MediaRecorder component.

---
+++ src/MediaRecorder/MediaRecorder.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './MediaRecorder.scss';
+
+const MediaRecorder = () => {
+    return (
+        <div className="container">
+            <h1>Media Recorder</h1>
+            <p>JavaScript, HTML and CSS.</p>
+        </div>
+    );
+};
+
+export default MediaRecorder;
